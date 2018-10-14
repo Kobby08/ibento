c6b44f43cb01200e27d2b120f5cb59c7a3ced8b2
Kobby <linusquam@gmail.com>
2018-10-14 22:14:37 +0000

    Adding association between user and events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,8 @@
 class EventsController < ApplicationController
 	
 	before_action :set_event, only: [:show, :edit, :update, :destroy]
-	
+	before_action :authenticate_user!, except: [:index, :show]
+	before_action :authorize_owner!, only: [:edit, :update, :destroy]
 	def index
 		@events = Event.all
 	end
@@ -12,12 +13,12 @@ class EventsController < ApplicationController
 
 	
 	def new
-		
 		@event = Event.new
 	end
 
 	def create
 		@event = Event.new(event_params)
+		@event.organizer = current_user
 		
 		if @event.save
 			flash[:notice] = "Event was created succesfully"
@@ -66,5 +67,14 @@ class EventsController < ApplicationController
 
 		def event_params
 			params.require(:event).permit(:title, :description, :start_date, :end_date, :venue, :location)
+		end
+
+		def authorize_owner!
+			authenticate_user!
+
+			unless @event.organizer == current_user
+				flash[:alert] = "You dont have permission to '#{action_name}' the event #{@event.title.upcase }"
+				redirect_to events_path
+			end
 		end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	has_many :organized_events, class_name: "Events", dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
