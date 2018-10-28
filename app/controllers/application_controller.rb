a9964e3b2a57b261b7634cc7268afcb090e3cf0c
Kobby <linusquam@gmail.com>
2018-10-28 20:28:28 +0000

    Adding strong parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
-end
+	
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+	private
+		def configure_permitted_parameters
+			devise_parameter_sanitizer.permit(:signup) do |user|
+				user.permit(:first_name, :last_name, :username, :organisation_name, :email, :password, :password_confirmation)
+			end
+			devise_parameter_sanitizer.permit(:account_update) do |user|
+				user.permit(:first_name, :last_name, :username, :organisation_name, :email, :password, :current_password, :password_confirmation)
+			end
+		end
+	end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
-	has_many :organized_events, class_name: "Events", dependent: :destroy
+	has_many :organized_events, class_name: "Event", dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  def to_s
+  	"#{username}"
+  end
 end
