4979358ee253eb1119107f43c266b5465cbb6191
Kobby <linusquam@gmail.com>
2018-11-20 14:14:19 +0000

    Adding users to comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   extend FriendlyId
   friendly_id :username, use: :slugged
 
-  
+  validates :username, presence: true, length: {minimum: 5}
 	has_many :organized_events, class_name: "Event", dependent: :destroy
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
