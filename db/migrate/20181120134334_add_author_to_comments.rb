772d13d6623e62b92d78376497b6a84c172abb69
Kobby <linusquam@gmail.com>
2018-11-20 15:23:24 +0000

    Twerking my code

--- db/migrate/20181120134334_add_author_to_comments.rb
+++
@@ -1,6 +0,0 @@
-class AddAuthorToComments < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :comments, :author, index: true
-    add_foreign_key :comments, :users, column: :author_id
-  end
-end
